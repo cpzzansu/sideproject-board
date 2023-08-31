80fbe7fe85d60195fe6a84cb4d9042be95dbcc07
yosep <dytpq1027@naver.com>
2023-08-31 16:48:31 +0900

    게시판 만들기 프로젝트

---
+++ src/main/java/com/fortyseven/sideprojectboard/SideprojectBoardApplication.java
@@ -0,0 +1,13 @@
+package com.fortyseven.sideprojectboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SideprojectBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SideprojectBoardApplication.class, args);
+    }
+
+}
